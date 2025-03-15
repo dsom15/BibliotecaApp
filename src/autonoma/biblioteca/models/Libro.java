d727df7985ada7e0dd1a494813d01d2600eb23f6
Jacobo Velez <vjacobo250@gmail.com>
2025-03-14 20:54:27 -0500

    Se agrego la clase libro

---
+++ src/autonoma/biblioteca/models/Libro.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author esteb
+ */
+public class Libro {
+    private  long id;
+    private String titulo;
+
+    public Libro(long id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+}
