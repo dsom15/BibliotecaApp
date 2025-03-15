5393cdb5248c4abaee65f2e2b026abc6970e2992
Juan diego montoya <solismontoya67@gmail.com>
2025-03-14 21:45:12 -0500

    Se crea la clase biblioteca y se ponen algunos metodos

--- src/autonoma/biblioteca/models/Biblioteca.java
+++ src/autonoma/biblioteca/models/Biblioteca.java
@@ -4,10 +4,68 @@
  */
 package autonoma.biblioteca.models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jeron
  */
 public class Biblioteca {
+    private Libro libro;
+    private Autor autor;
+    private ArrayList <Libro>libros;
+
+    public Biblioteca(Libro libro, Autor autor,ArrayList <Libro>libros) {
+        this.libro = libro;
+        this.autor = autor;
+        this.libros = new ArrayList<>();
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
     
-}
+    public ArrayList<Libro> getLibros(){
+        return libros;
+    }
+    
+    
+    /** METODOS
+     * se crea el metodo mostrar
+     */
+     
+    public void mostarLibro(){
+        System.out.println("Libro"+libro.getTitulo());
+    }
+    
+    public void agregarLibro(Libro libro){     
+            
+            libros.add(libro);           
+        
+                    
+    }
+    
+    public Libro  buscarLibro (long idLibro){
+       for (Libro libro: libros){
+          if (libro.getId()==idLibro){
+              return libro;
+          }
+       }
+       return null;
+       
+    }
+        
+ } 
+
